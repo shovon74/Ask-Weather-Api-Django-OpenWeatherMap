7f80861e9cff92da8a026f5fb2a34727c972f9f3
Shib Shankar Bose <shibshankarbose71@gmail.com>
2019-09-17 12:42:52 +0600

    Forms added for adding cities

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,25 +1,40 @@
 import requests
 from django.shortcuts import render
-
+from .models import City
+from .forms import CityForm
 
 def index(request):
 
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=e2982aab2c245465921e2cde7c21cc4e'
 
-	city = 'Dhaka'
 
-	r=requests.get(url.format(city)).json()
-	# print(r)
+	if request.method == 'POST':
+		form = CityForm(request.POST)
+		form.save()
 
-	city_weather ={
-		'city': city,
-		'temparature': r['main']['temp'],
-		'description':r['weather'][0]['description'],
-		'icon': r['weather'][0]['icon'],
-	}
+	form = CityForm()
 
-	# print(city_weather)
-	context = {'city_weather': city_weather}
+	cities = City.objects.all()
+
+	weather_data = []
+	
+	for city in cities:
+		r=requests.get(url.format(city)).json()
+		
+
+		city_weather ={
+			'city': city.name,
+			'temparature': r['main']['temp'],
+			'description':r['weather'][0]['description'],
+			'icon': r['weather'][0]['icon'],
+		}
+		weather_data.append(city_weather)
+
+
+
+	print(weather_data)
+	context = {'weather_data': weather_data, 'form': form}
+	print(context)
 	return render(request, 'weatherapp/index.html', context)
 
 # Create your views here.
