b79f3125b92dd04429f01f97b7034107fab23b06
Shib Shankar Bose <shibshankarbose71@gmail.com>
2022-07-23 13:29:16 +0600

    created local config file

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,75 +1,77 @@
 import requests
 import configparser
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
-config_file = r'C:\Users\GP.GP-PC-5001819\Desktop\AskWeather\src\config_file.conf'
+config_file_path = r'C:\\Users\\GP.GP-PC-5001819\\Desktop\AskWeather\\src\\'
+
+config_file = config_file_path + 'local_config_file.conf'
 
 config = configparser.ConfigParser()
 config.read(config_file)
 
 
 def index(request):
-	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+config['API_KEY']['KEY']
-	print(url)
-	# import pdb
-	# pdb.set_trace()
-	err_msg = ''
-	message = ''
-	message_class = ''
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + config['API_KEY']['KEY']
+    print(url)
+    # import pdb
+    # pdb.set_trace()
+    err_msg = ''
+    message = ''
+    message_class = ''
 
-	if request.method == 'POST':
-		form = CityForm(request.POST)
+    if request.method == 'POST':
+        form = CityForm(request.POST)
 
-		if form.is_valid():
-			new_city = form.cleaned_data['name']
-			existing_city_count = City.objects.filter(name=new_city).count()
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
 
-			if existing_city_count == 0:
-				r = requests.get(url.format(new_city)).json()
-				# print(r)
-				if r['cod'] == 200:
-					form.save()
-				else:
-					err_msg = "City does not exist"
-			else:
-				err_msg = "City Already exists"
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                # print(r)
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = "City does not exist"
+            else:
+                err_msg = "City Already exists"
 
-		if err_msg:
-			message = err_msg
-			message_class = 'is-danger'
-		# else:
-		# 	message = 'City added successfully'
-		# 	message_class = 'is-success'
-	# print(err_msg)
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+    # else:
+    # 	message = 'City added successfully'
+    # 	message_class = 'is-success'
+    # print(err_msg)
 
-	form = CityForm()
+    form = CityForm()
 
-	cities = City.objects.all()
-	# print(cities)
-	weather_data = []
-	
-	for city in cities:
-		r=requests.get(url.format(city)).json()
-		print(r)
-		city_weather ={
-			'city': city.name,
-			'temperature': r['main']['temp'],
-			'description': r['weather'][0]['description'],
-			'icon': r['weather'][0]['icon'],
-		}
-		weather_data.append(city_weather)
+    cities = City.objects.all()
+    print(cities)
+    weather_data = []
 
-	context = {
-		'weather_data': weather_data,
-		'form': form,
-		'message': message,
-		'message_class':message_class
-	}
-	return render(request, 'weatherapp/index.html', context)
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        # print(r)
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {
+        'weather_data': weather_data,
+        'form': form,
+        'message': message,
+        'message_class': message_class
+    }
+    return render(request, 'weatherapp/index.html', context)
 
 
 def delete_city(request, city_name):
-	City.objects.get(name=city_name).delete()
-	return redirect('home')
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
